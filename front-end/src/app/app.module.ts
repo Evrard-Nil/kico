0fb03ff1439fa546d694e701e4b5b6b786f16141
Thomas Herbelin <herbelin.thomas@gmail.com>
2020-10-27 10:21:59 +0100

    feat: add segmentation component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ACSegmentationComponent } from './components/acsegmentation/acsegmentation.component';
+import { ACHeaderComponent } from './components/acheader/acheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ACSegmentationComponent,
+    ACHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/components/acsegmentation/acsegmentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acsegmentation',
+  templateUrl: './acsegmentation.component.html',
+  styleUrls: ['./acsegmentation.component.css']
+})
+export class ACSegmentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
