2d3e487cd38c4341218d8dc94cfebbd5927b1329
Thomas Herbelin <herbelin.thomas@gmail.com>
2020-10-27 10:23:22 +0100

    chrores: modify config and add forget file

---
+++ front-end/src/app/components/acheader/acheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acheader',
+  templateUrl: './acheader.component.html',
+  styleUrls: ['./acheader.component.css']
+})
+export class ACHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
