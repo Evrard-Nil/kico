be30626513c593b851bc3a8d04058f1180e3f300
Thomas Herbelin <herbelin.thomas@gmail.com>
2020-11-03 18:05:54 +0100

    feat: add model

--- front-end/src/app/model/image.ts
+++ front-end/src/app/model/image.ts
@@ -1,10 +1,12 @@
 export class Image {
-    constructor(
-        public Secteur_id :string,
-        public secteur_name :string,
-        public name : String,
-        public time :string,
-        public video_id : string
-      ) { }
-  
+  id: number
+  Secteur_id: number
+  secteur_name: string
+  name: String
+  time: string
+  video_id: number
+
+  constructor(
+
+  ) {}
 }
---
+++ front-end/src/app/model/video.ts
@@ -0,0 +1,12 @@
+export class Video {
+  id: string
+  annotatedBy: string
+  date: Date
+  scorePci: number
+  state: string
+  url: string
+  path: string
+  title: string
+  constructor() {}
+
+}
