1727767d1634218a046506deb8b5943618068a9d
Thomas Herbelin <herbelin.thomas@gmail.com>
2020-11-17 10:34:55 +0100

    refacto : modify model
    Delete Path property and correct a typo error

--- front-end/src/app/model/image.ts
+++ front-end/src/app/model/image.ts
@@ -1,12 +1,10 @@
 export class Image {
   id: number
-  Secteur_id: number
+  secteur_id: number
   secteur_name: string
   name: String
   time: string
   video_id: number
 
-  constructor(
-
-  ) {}
+  constructor() {}
 }
