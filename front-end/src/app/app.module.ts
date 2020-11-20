0ecb19cece544871172e1eaf36236a336cf48a57
samylegalloudec <samy.legalloudec@gmail.com>
2020-11-20 14:01:58 +0100

    ajout composant view

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ACImageAnnotatorComponent } from './components/ac-image-annotator/ac-image-annotator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ACImageViewComponent } from './components/ac-image-view/ac-image-view.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ACImageAnnotatorComponent
+    ACImageAnnotatorComponent,
+    ACImageViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
