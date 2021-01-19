d2a41fc5eb1381d5f34f1c635c5194cc23bf58c1
Thomas Herbelin <herbelin.thomas@gmail.com>
2021-01-19 17:07:18 +0100

    fix: wrong verb

--- front-end/src/app/services/api.service.ts
+++ front-end/src/app/services/api.service.ts
@@ -25,7 +25,7 @@ export class ApiService {
   }
 
   doPut<T>(endpoint: string, body: any, options?: object) {
-    return this.http.post<T>(endpoint, body, options)
+    return this.http.put<T>(endpoint, body, options)
     .pipe(
       catchError(this.handleError)
     )
