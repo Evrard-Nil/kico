be8c7e04373d341b67ebfb10baa141c866d9636a
Thomas Herbelin <herbelin.thomas@gmail.com>
2021-01-19 14:49:29 +0100

    fix: modify backend url

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api/v1'
+  apiBaseUrl: 'http://localhost:8080/api/v1',
+  fileBaseUrl: 'http://localhost:8082' 
 };
