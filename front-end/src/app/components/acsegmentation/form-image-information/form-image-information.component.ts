88bd5495885c547a733645e82af50d1bc2cc036b
Thomas Herbelin <herbelin.thomas@gmail.com>
2021-01-22 15:46:28 +0100

    feat: add save image button

--- front-end/src/app/components/acsegmentation/form-image-information/form-image-information.component.ts
+++ front-end/src/app/components/acsegmentation/form-image-information/form-image-information.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Image } from 'src/app/model/image';
 import { Secteurs } from 'src/assets/data/secteurs'
+import { ImageService } from 'src/app/services/image.service';
 
 @Component({
   selector: 'app-form-image-information',
@@ -14,7 +15,7 @@ export class FormImageInformationComponent implements OnInit {
   @Output() eventDeleteImage : EventEmitter<Image>;
   secteurs = Secteurs
 
-  constructor() {
+  constructor(private imageService: ImageService) {
 		this.eventNameImage = new EventEmitter();
 		this.eventDeleteImage = new EventEmitter();
   }
@@ -30,6 +31,13 @@ export class FormImageInformationComponent implements OnInit {
   onChangeName(newName: String){
     this.currentImage.name = newName;
     this.eventNameImage.emit(this.currentImage)
+  }
+
+  onUpdateImage() {
+    console.log("UPDATE...",this.currentImage)
+    this.imageService.updateImage(this.currentImage).subscribe((image) => {
+      console.log("UPDATED : ", image)
+    })
   }
 
   
