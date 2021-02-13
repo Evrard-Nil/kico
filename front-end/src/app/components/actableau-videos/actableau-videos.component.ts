19ee9df82aa6e9992b85c45ca1dec1917c09a686
Thomas Herbelin <herbelin.thomas@gmail.com>
2021-02-13 18:06:10 +0100

    fix: upload video working

--- front-end/src/app/components/actableau-videos/actableau-videos.component.ts
+++ front-end/src/app/components/actableau-videos/actableau-videos.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Video } from 'src/app/model/video';
-import { VideoService } from 'src/app/services/video.services';
+import { VideoService } from 'src/app/services/video.service';
 import { Router } from '@angular/router';
 import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
 import { of } from 'rxjs';
@@ -9,37 +9,20 @@ import { catchError, map } from 'rxjs/operators';
 @Component({
   selector: 'app-actableau-videos',
   templateUrl: './actableau-videos.component.html',
-  styleUrls: ['./actableau-videos.component.css']
+  styleUrls: ['./actableau-videos.component.css'],
 })
 export class ActableauVideosComponent implements OnInit {
-  @ViewChild('fileUpload', { static: false }) fileUpload: ElementRef; files = [];
+  @ViewChild('inputVideo') fileUpload: ElementRef;
+  files = [];
 
   videos: Array<Video>;
 
-  constructor(private videoService: VideoService, private router: Router) { this.videos = new Array<Video>(); }
+  constructor(private videoService: VideoService, private router: Router) {
+    this.videos = new Array<Video>();
+  }
 
   ngOnInit(): void {
     this.loadVideos();
-    /* this.videos.push({
-       id: '91',
-       score_pci: 12,
-       annotated_by: 'Jean',
-       date: new Date(),
-       state: 'Annoté',
-       title: 'test',
-       url: 'url',
-       path: 'path',
-     }, {
-       id: '92',
-       score_pci: 15,
-       annotated_by: 'Pierre',
-       date: new Date(),
-       state: 'Annoté',
-       title: 'test',
-       url: 'url',
-       path: 'path',
-     }
-     );*/
   }
 
   deleteVideo(id): void {
@@ -48,40 +31,21 @@ export class ActableauVideosComponent implements OnInit {
   }
 
   addVideo(): void {
-    console.log('add video');
-
-    let video = new Video();
-    video.id = '92';
-    video.score_pci = 15;
-    video.annotated_by = 'Pierre';
-    video.date = new Date();
-    video.state = 'annotated';
-    video.title = 'test';
-    //video.url = '/videos/b50905c7-de74-40c6-9d7f-bbedafc98c9g';// /videos/{id}
-    //video.path = '';// 
-
-
-    console.log('add video2');
-    /*
-        const formData = new FormData()
-        formData.append('name', "Nouvelle image (" + this.getDurationFromSeconds(time) + ")");
-        formData.append('fileName', blob);
-        formData.append('secteur_id', "0");
-        formData.append('time', this.getDurationFromSeconds(time));
-    
-        this.videoService.addVideo(formData).subscribe((video) => {
-          image.url = environment.fileBaseUrl + image.url
-          this.eventCreateImage.emit(image)
-        })*/
-
-    this.videoService.addVideo(video);/*
-      .subscribe((videos) => {
-        console.log('VIDEOS RECUE :', videos);
-
+    this.files = this.fileUpload.nativeElement.files;
+    for (let i = 0; i < this.files.length; i++) {
+      var formData = this.createFormData(this.files[i]);
+      this.videoService.addVideo(formData).subscribe((video) => {
+        // TODO : Update du component pour ne pas avoir à recharger la page pour l'affichage de la vidéo ?
+        console.log(video);
       });
-*/
-    console.log('add video3');
+    }
+  }
 
+  createFormData(file: File) {
+    const formData = new FormData();
+    formData.append('title', file.name);
+    formData.append('fileName', file);
+    return formData;
   }
 
   segmentate(id): void {
@@ -98,17 +62,15 @@ export class ActableauVideosComponent implements OnInit {
    * Charge les vidéos depuis le serveur
    */
   loadVideos(): void {
-    this.videoService.getVideos()
-      .subscribe((videos) => {
-        console.log('VIDEOS RECUE :', videos);
-        videos.forEach((video) => {
-          console.log('video loaded:');
-          console.log(video);
+    this.videoService.getVideos().subscribe((videos) => {
+      console.log('VIDEOS RECUE :', videos);
+      videos.forEach((video) => {
+        console.log('video loaded:');
+        console.log(video);
 
-          this.videos.push(video);
-        });
-        console.log(videos);
+        this.videos.push(video);
       });
+      console.log(videos);
+    });
   }
-
 }
