b71e0187bface0ec448248e7036d71f24dd709e9
Thomas Herbelin <herbelin.thomas@gmail.com>
2021-02-17 11:23:25 +0100

    fix : size of the image (1280x720)

---
+++ front-end/src/app/app.constants.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppConstants {
+
+    public image = {
+        WIDTH : 1280,
+        HEIGHT : 720
+    }
+    
+}
